35e95597bfc5721f0aa9c48f16b40e1667771c6a
szhanggit <zhangyidingchina@hotmail.com>
2026-06-01 19:26:32 -0400

    Add shoe hub.

--- 06.monitor/terraform/main.tf
+++ 06.monitor/terraform/main.tf
@@ -139,6 +139,13 @@ resource "aws_security_group" "app-server-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 8020
+        to_port = 8020
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
