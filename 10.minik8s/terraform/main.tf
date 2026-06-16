cfdbd89b0723f6b44f2d0df5f3f8dc89099c5d79
szhanggit <zhangyidingchina@hotmail.com>
2026-06-16 12:31:38 -0400

    Added to be four worker nodes.

--- 10.minik8s/terraform/main.tf
+++ 10.minik8s/terraform/main.tf
@@ -170,7 +170,7 @@ resource "aws_instance" "k8s-master" {
     }
 }
 
-resource "aws_instance" "k8s-node" {
+resource "aws_instance" "k8s-node-0" {
     ami                         = data.aws_ami.latest-ubuntu-image.id
     instance_type               = "t3.small"
     subnet_id                   = aws_subnet.myapp-subnet-1.id
@@ -180,7 +180,49 @@ resource "aws_instance" "k8s-node" {
     key_name                    = aws_key_pair.ssh-key.key_name
     user_data                   = file("entry-script.sh")
     tags = {
-        Name = "K8s Node"
+        Name = "K8s Node 0"
+    }
+}
+
+resource "aws_instance" "k8s-node-1" {
+    ami                         = data.aws_ami.latest-ubuntu-image.id
+    instance_type               = "t3.small"
+    subnet_id                   = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids      = [aws_security_group.node-sg.id]
+    availability_zone           = var.avail_zone_1
+    associate_public_ip_address = true
+    key_name                    = aws_key_pair.ssh-key.key_name
+    user_data                   = file("entry-script.sh")
+    tags = {
+        Name = "K8s Node 1"
+    }
+}
+
+resource "aws_instance" "k8s-node-2" {
+    ami                         = data.aws_ami.latest-ubuntu-image.id
+    instance_type               = "t3.small"
+    subnet_id                   = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids      = [aws_security_group.node-sg.id]
+    availability_zone           = var.avail_zone_1
+    associate_public_ip_address = true
+    key_name                    = aws_key_pair.ssh-key.key_name
+    user_data                   = file("entry-script.sh")
+    tags = {
+        Name = "K8s Node 2"
+    }
+}
+
+resource "aws_instance" "k8s-node-3" {
+    ami                         = data.aws_ami.latest-ubuntu-image.id
+    instance_type               = "t3.small"
+    subnet_id                   = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids      = [aws_security_group.node-sg.id]
+    availability_zone           = var.avail_zone_1
+    associate_public_ip_address = true
+    key_name                    = aws_key_pair.ssh-key.key_name
+    user_data                   = file("entry-script.sh")
+    tags = {
+        Name = "K8s Node 3"
     }
 }
 
@@ -201,6 +243,18 @@ output "master_private_ip" {
     value = aws_instance.k8s-master.private_ip
 }
 
-output "node_public_ip" {
-    value = aws_instance.k8s-node.public_ip
+output "node_0_public_ip" {
+    value = aws_instance.k8s-node-0.public_ip
+}
+
+output "node_1_public_ip" {
+    value = aws_instance.k8s-node-1.public_ip
+}
+
+output "node_2_public_ip" {
+    value = aws_instance.k8s-node-2.public_ip
+}
+
+output "node_3_public_ip" {
+    value = aws_instance.k8s-node-3.public_ip
 }
